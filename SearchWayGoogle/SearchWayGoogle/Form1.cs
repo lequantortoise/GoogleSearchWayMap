4d5581d7e7818ac2493816cf9a77aac79fd3a3b4
quanvan <qb.yellow@gmail.com>
2016-11-05 21:27:08 +0900

    first commit ever

---
+++ SearchWayGoogle/SearchWayGoogle/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SearchWayGoogle
+{
+    public partial class GoogleMapFormSearch : Form
+    {
+        public GoogleMapFormSearch()
+        {
+            InitializeComponent();
+        }
+    }
+}
